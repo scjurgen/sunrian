8046d7187840e799c8851b04a3fe5dc35cc77204
Jurgen Schwietering <scjurgen@yahoo.com>
2013-12-25 15:32:30 +0100

    Initial Commit

---
+++ Sunrian/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sunrian
+//
+//  Created by jay on 25/12/13.
+//  Copyright (c) 2013 Jurgen Schwietering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sunrian/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sunrian
+//
+//  Created by jay on 25/12/13.
+//  Copyright (c) 2013 Jurgen Schwietering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
